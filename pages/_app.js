e4cb3e9d3d2d5ec9dd2458d36bccf0cab0790c48
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-17 21:02:13 +0600

    header & footer for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import Email from '../components/Email'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Social from '../components/Social'
 import '../styles/globals.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+    <Social />
+    <Email />
+    <ToastContainer
+      position="top-center"
+      theme="dark"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  </>
 }
 
 export default MyApp
