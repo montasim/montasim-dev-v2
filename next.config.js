60f4e982f3218c092ae91c55b538f04439c6c5f5
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-17 20:39:51 +0600

    project section

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i.ibb.co'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { BiRightArrow } from 'react-icons/bi';
+import { FaRegFolder } from 'react-icons/fa';
+import { FiGithub, FiExternalLink } from 'react-icons/fi';
 import professionalPhoto from '../assets/images/SHUVO Transparent BG.png'
 import Email from '../components/Email'
 import Footer from '../components/Footer'
@@ -28,6 +32,21 @@ export default function Home() {
   ];
 
   const skills = ['JavaScript (ES6+)', 'React JS', 'Node.js', 'Express.js', 'Tailwind CSS', 'BootStrap'];
+
+  const [projects, setProjects] = useState([]);
+  const [noteWorthyProjects, setNoteWorthyProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('projects.json')
+      .then(res => res.json())
+      .then(data => setProjects(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('noteWorthyProjects.json')
+      .then(res => res.json())
+      .then(data => setNoteWorthyProjects(data));
+  }, []);
 
   return (
     <div>
@@ -111,6 +130,92 @@ export default function Home() {
                 <Image src={professionalPhoto} alt="" />
               </div>
             </div>
+          </div>
+        </section>
+
+        <section id='projects' className="min-h-screen">
+          <div className="">
+            <div className='flex items-center gap-6'>
+              <h3 className='lg:text-3xl md:text-3xl text-2xl text-accent font-bold'>Some Things I’ve Built </h3>
+              <hr className='bg-[#233554] lg:w-80 md:w-64 w-40 h-0.5'></hr>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 mt-8 sm:gap-y-4"
+          >
+            {
+              projects?.map((project) =>
+                <div key={project._id} className='flex flex-col md:flex-row lg:flex-row items-center justify-between gap-y-8 my-8'>
+                  <Image
+                    width={500}
+                    height={500}
+                    alt={project.projectName + ' home page'}
+                    src={project.projectImg}
+                    className="object-cover rounded-md lg:w-8/12 md:w-7/12 w-full"
+                  />
+
+                  <div className='lg:w-5/12 md:w-5/12 w-full'>
+                    <div className='text-right'>
+                      <p className='lg:text-lg md:text-lg text-md text-primary mb-2'>{project.projectCategory} Project</p>
+                      <h4 className='lg:text-2.5xl md:text-2.5xl text-2xl font-bold text-accent'>{project.projectName}</h4>
+                    </div>
+
+                    <div className='bg-[#112240] p-6 text-right my-6 lg:ml-[-100px] md:lg:ml-[-100px] ml-0 rounded'>
+                      <p>{project.projectDescription.slice(0, 140)}</p>
+                    </div>
+
+                    <div className='flow-root'>
+                      <div className='flex gap-x-6 float-right mb-8 text-[#a8b2d1]'>
+                        <p>{project.technologyUsed}</p>
+                      </div>
+                    </div>
+
+                    <div className='flow-root'>
+                      <div className='grid grid-cols-2 gap-x-6 float-right text-2xl text-accent'>
+                        <a href={project.clientSideCodeLink} target='_blank' rel="noreferrer" className='hover:text-primary'><FiGithub /></a>
+                        <a href={project.liveWebsiteLink} target='_blank' rel="noreferrer" className='hover:text-primary'><FiExternalLink /></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>)
+            }
+          </div>
+
+          <div className='text-center mt-14'>
+            <h2 className='text-3xl font-bold text-accent mb-6'>Other Noteworthy Projects</h2>
+            <Link href='/archive' className='text-lg text-primary hover:underline transition duration-700 ease-in-out'>view the archive</Link>
+          </div>
+
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-20'>
+            {
+              noteWorthyProjects?.map((project) =>
+                <div key={project._id} className="h-96 bg-[#112240] rounded-md p-6">
+                  <div className='flex justify-between items-center mb-8'>
+                    <FaRegFolder className='text-5xl text-primary' />
+
+                    <div className='grid grid-cols-2 gap-x-6 float-right text-2xl text-accent'>
+                      <a href={project.clientSideCodeLink} target='_blank' rel="noreferrer" className='hover:text-primary'><FiGithub /></a>
+                      <a href={project.liveWebsiteLink} target='_blank' rel="noreferrer" className='hover:text-primary'><FiExternalLink /></a>
+                    </div>
+                  </div>
+
+                  <div>
+                    <h4 className='text-xl text-accent font-bold'>{project.projectName}</h4>
+
+                    <p className='mt-4 mb-8'>
+                      {project.projectDescription.slice(0, 180)}
+                    </p>
+
+                    <div className='flex gap-x-6'>
+                      <small>{project.technologyUsed}</small>
+                    </div>
+                  </div>
+                </div>)
+            }
+          </div>
+
+          <div className='flex justify-center items-center mt-10'>
+            <button className="btn rounded-xs border-primary text-primary px-10 capitalize text-md mt-14 hover:bg-primary hover:opacity-70 hover:text-black" > Show All</button >
           </div>
         </section>
       </main>
