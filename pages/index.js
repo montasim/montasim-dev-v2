19eaf9661e112c29f7e41ff972f6ae85c8e994fb
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-17 21:18:21 +0600

    bring text forword to image

--- pages/index.js
+++ pages/index.js
@@ -170,7 +170,7 @@ export default function Home() {
                     className="object-cover rounded-md lg:w-8/12 md:w-7/12 w-full"
                   />
 
-                  <div className='lg:w-5/12 md:w-5/12 w-full'>
+                  <div className='lg:w-5/12 md:w-5/12 w-full z-50'>
                     <div className='text-right'>
                       <p className='lg:text-lg md:text-lg text-md text-primary mb-2'>{project.projectCategory} Project</p>
                       <h4 className='lg:text-2.5xl md:text-2.5xl text-2xl font-bold text-accent'>{project.projectName}</h4>
