d2e5424bf6a53a70c8c05f85f6facedff3893529
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-17 19:30:27 +0600

    social media icons

---
+++ components/Social.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillLinkedin, AiFillGithub, AiFillFacebook } from 'react-icons/ai';
+
+const Social = () => {
+    const socialLinks = [
+        {
+            "id": 1,
+            "route": "https://www.linkedin.com/in/montasim",
+            "icon": <AiFillLinkedin />
+        },
+        {
+            "id": 2,
+            "route": "https://github.com/montasim",
+            "icon": <AiFillGithub />
+        },
+        {
+            "id": 3,
+            "route": "https://www.facebook.com/montasimmamun/",
+            "icon": <AiFillFacebook />
+        }
+    ];
+
+    return (
+        <section className='fixed bottom-0 left-0 ml-10 mr-20 px-2 hidden lg:block md:block'>
+            <div className='flex flex-col gap-y-6 justify-center'>
+                {
+                    socialLinks.map(socialLink =>
+                        <Link key={socialLink.id} className='text-2xl hover:text-primary' href={socialLink.route} target='_blank' rel="noreferrer">{socialLink.icon}</Link>)
+                }
+                <div className='bg-white h-20 w-0.5 ml-[10px]'></div>
+            </div>
+        </section>
+    );
+};
+
+export default Social;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Social from '../components/Social'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
       </main>
 
       <Footer />
+      <Social />
     </div>
   )
 }
