49be922469b622bd157adff7074cccffd11f3aaa
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-17 21:57:24 +0600

    key is changed to index

--- pages/index.js
+++ pages/index.js
@@ -88,8 +88,8 @@ export default function Home() {
               className='mt-8 border-2 border-white border-opacity-25 divide-y-2 divide-white sm:grid sm:divide-y-0 sm:divide-x-2 sm:border-l-0 sm:border-r-0 divide-opacity-25 sm:grid-cols-3 text-center'
             >
               {
-                summary.map(s =>
-                  <li key={s.id} className='p-8'>
+                summary.map((s, index) =>
+                  <li key={index} className='p-8'>
                     <p className='text-3xl font-extrabold text-primary'>{s.number}+</p>
                     <p className='mt-1 text-xl font-medium'>{s.name}</p>
                   </li>)
@@ -203,7 +203,7 @@ export default function Home() {
 
           <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-20'>
             {
-              noteWorthyProjects?.map((project) =>
+              noteWorthyProjects?.map(project =>
                 <div key={project._id} className="h-96 bg-[#112240] rounded-md p-6">
                   <div className='flex justify-between items-center mb-8'>
                     <FaRegFolder className='text-5xl text-primary' />
